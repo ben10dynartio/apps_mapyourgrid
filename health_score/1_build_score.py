9d0b46226ab21e7118915d9f39294f57f828279a
B.Ribon <b.ribon@dynartio.com>
2025-09-18 23:11:21 +0200

    Upgrade

---
+++ health_score/1_build_score.py
@@ -0,0 +1,214 @@
+import geopandas as gpd
+import pandas as pd
+import networkx as nx
+import json
+import pprint
+from config import WORLD_COUNTRY_DICT
+
+SHOW_SMALL_GRAPHSET = True
+
+COUNTRY_CODE = "CO"
+DATA_FOLDER = "data"
+DATA_FOLDER = "/home/ben/DevProjects/osm-power-grid-map-analysis/data"
+
+
+def connectivity_analysis(graph):
+    stats = {}
+    list_graph_subsets = list(nx.connected_components(graph))
+    graph_stats = []
+    try:
+        for l in list_graph_subsets:
+            nbsub = len(
+                [n for n in l if (graph.nodes[n]["grid_role"] == "substation") and (graph.nodes[n]["status"] != "disconnected")])
+            nbseg = len([e for e in graph.subgraph(l).edges if graph.edges[e]["status"] != "disconnected"])
+            if nbsub:
+                graph_stats.append({"nbsub": nbsub, "nbseg": nbseg})
+            """if nbsub<10 and nbsub and SHOW_SMALL_GRAPHSET:
+                print(f"-- SECTION {nbsub} x {nbseg} :")
+                [print(n, end=" | ") for n in l if
+                 (graph.nodes[n]["grid_role"] == "substation") and (graph.nodes[n]["status"] != "disconnected")]
+                print()"""
+    except Exception:
+        stats["substation_connectivity"] = -1
+        stats["substation_connectivity_pct"] = -1
+        return stats
+
+    df_stat = pd.DataFrame(graph_stats)
+    # stats["substation_connectivity_pct"] = "{:.1f}".format((max(df_stat["nbsub"]) / sum(df_stat["nbsub"]))*100)
+
+    ## ANALYSIS OF GRID CONNECTIVITY
+    if len(df_stat) == 0:
+        stats["substation_connectivity"] = 0
+        stats["substation_connectivity_pct"] = 0
+    else:
+        df_stat = df_stat.sort_values(["nbsub", "nbseg"], ascending=False)
+        # Compute substation connectivity
+        cumsum = 0
+        for i, row in enumerate(df_stat.to_dict(orient='records')):
+            cumsum += row["nbsub"] / (i + 1)
+        stats["substation_connectivity_pct"] = cumsum / sum(df_stat["nbsub"])
+
+        df_stat_text = df_stat["nbsub"].astype(str) + "x" + df_stat["nbseg"].astype(str)
+        counts = df_stat_text.value_counts()
+        stats["substation_connectivity"] = " + ".join(
+            [f"{counts[subseg]}^({subseg})" if counts[subseg] != 1 else f"{subseg}"
+             for subseg in df_stat_text.unique().tolist()])
+    # print(counts)
+    # stats["grid_connectivity"] = " + ". join(f"{x['nbsub']}x{x['nbseg']}" for x in df_stat.to_dict(orient='records'))
+
+    #print("  -- Substation connectivity = ", stats["substation_connectivity"])
+    #print("  -- Substation connectivity % = ", stats["substation_connectivity_pct"], "%")
+    return stats
+
+def main(country_code):
+    df_power_line = gpd.read_file(f"{DATA_FOLDER}/{country_code}/osm_brut_power_line.gpkg")
+    df_power_tower = gpd.read_file(f"{DATA_FOLDER}/{country_code}/osm_brut_power_tower_transition.gpkg")
+    df_power_substation = gpd.read_file(f"{DATA_FOLDER}/{country_code}/osm_clean_power_substation.gpkg")
+    df_pregraph_power_nodes = gpd.read_file(f"{DATA_FOLDER}/{country_code}/pre_graph_power_nodes.gpkg")
+    df_pregraph_power_lines = gpd.read_file(f"{DATA_FOLDER}/{country_code}/pre_graph_power_lines.gpkg")
+    gdf_postgraph_nodes = gpd.read_file(f"{DATA_FOLDER}/{country_code}/post_graph_power_nodes.gpkg")
+    gdf_postgraph_lines = gpd.read_file(f"{DATA_FOLDER}/{country_code}/post_graph_power_lines.gpkg")
+
+    gdf_postgraph_lines_circuit = gpd.read_file(f"{DATA_FOLDER}/{country_code}/post_graph_power_lines_circuit.gpkg")
+    gdf_postgraph_nodes_circuit = gpd.read_file(f"{DATA_FOLDER}/{country_code}/post_graph_power_nodes_circuit.gpkg")
+
+    G = nx.MultiGraph()
+    gdf_postgraph_nodes.apply(lambda node: G.add_node(node["osmid"], grid_role=node["grid_role"],
+                                            status=node["status"]), axis=1)
+    gdf_postgraph_lines.apply(lambda line: G.add_edge(line["node0"], line["node1"], status= line["status"],
+                                            osmid=line["osmid"]), axis=1)
+
+    Gcircuit = nx.MultiGraph()
+    gdf_postgraph_nodes_circuit.apply(lambda node: Gcircuit.add_node(node["osmid"], grid_role=node["grid_role"],
+                                            status=node["status"]), axis=1)
+    gdf_postgraph_lines_circuit.apply(lambda line: Gcircuit.add_edge(line["node0"], line["node1"], status= line["status"],
+                                            osmid=line["osmid"]), axis=1)
+    if not len(gdf_postgraph_lines):
+        return
+
+    mystat_classic = connectivity_analysis(G)
+    mystat_circuit = connectivity_analysis(Gcircuit)
+
+    # count nb connection substation <-> line
+    nb_conn_line_sub = 0
+    for node in G.nodes:
+        mynode = G.nodes[node]
+        if mynode["grid_role"] == "substation":
+            nb_conn_line_sub += G.degree[node]
+        else:
+            pass
+
+
+    with open(f"build/osmose_{country_code}.json") as f:
+        data = json.load(f)
+
+    sum_osmose = sum(list(data["class"].values()))
+    if sum_osmose == 0:
+        return
+
+    ## Computing Health indicator
+    names = {}
+    indicators = {}
+    explanations = {}
+
+
+    key = "health_power_line_connectivity"
+    names[key] = "Line connectivity"
+    indicators[key] = len(gdf_postgraph_lines[gdf_postgraph_lines["status"] == "connected"]) / len(gdf_postgraph_lines)
+    explanations[key] = "nb(Grid connected power line|cable) / nb(Grid power line|cable) || Grid line derivated (~=) from OSM power=line|cable after connectivity analysis"
+
+    key = "health_grid_connectivity_without_circuit"
+    names[key] = "Grid overall connectivity (without circuits)"
+    indicators[key] = mystat_classic["substation_connectivity_pct"]
+    explanations[key] = "Grid analysis"
+
+    key = "health_grid_connectivity_with_circuit"
+    names[key] = "Grid overall connectivity (with circuits)"
+    indicators[key] = mystat_circuit["substation_connectivity_pct"]
+    explanations[key] = "Grid analysis"
+
+    key = "health_line_voltage_completeness"
+    names[key] = "Voltage attribute completeness on power lines and cables"
+    indicators[key] = len(df_power_line[df_power_line["voltage"].notnull()]) / len(df_power_line)
+    explanations[key] =  "nb(OSM power=line|cable & voltage!='') /nb(OSM power=line|cable)"
+
+    if len(df_power_line) > 0:
+        key = "health_line_cables_completness"
+        names[key] = "Cables attribute completeness on power lines and cables"
+        indicators[key] = len(df_power_line[df_power_line["cables"].notnull()]) / len(df_power_line)
+        explanations[key] =  "nb(OSM power=line|cable & cables!='') /nb(OSM power=line|cable)"
+
+    if len(df_power_substation) > 0:
+        key = "health_substation_voltage_completness"
+        names[key] = "Voltage attribute completeness on substations"
+        indicators[key] = len(df_power_substation[df_power_substation["voltage"].notnull()]) / len(df_power_substation)
+        explanations[key] = "nb(OSM power=substation & voltage!='')/nb(OSM power=substation)"
+
+    key = "health_connected_power_tower"
+    names[key] = "Connected power tower (Osmose&nbsp;Class&nbsp;1)"
+    indicators[key] = 1 - data["class-extend"]["nb_lone_power_tower"] / (len(df_power_tower[df_power_tower["power"]=="tower"]))
+    explanations[key] = "1 - nb(Osmose-Class1-power=tower) / nb(OSM power=tower)"
+
+    key = "health_complete_power_line"
+    names[key] = "Complete power line (Osmose&nbsp;Class&nbsp;2)"
+    indicators[key] = 1 - data["class"]["2"] / (len(df_power_line[df_power_line["power"]=="line"])*2)
+    explanations[key] = "1 - nb(Osmose-Class2) / 2*nb(OSM power=line)"
+
+    if len(df_pregraph_power_nodes[df_pregraph_power_nodes["grid_role"].isin(["to_international", "lambda_node"])]) > 0:
+        print("LEN grid_role =", len(df_pregraph_power_lines))
+        print("data class 3 =", data["class"]["3"])
+        key = "health_consistent_line_voltage_connection"
+        names[key] = "Line voltage consistency (Osmose&nbsp;Class&nbsp;3)"
+        indicators[key] = 1 - data["class"]["3"] / (len(df_pregraph_power_nodes[df_pregraph_power_nodes["grid_role"].isin(["to_international", "lambda_node"])]))
+        explanations[key] = "1 - nb(Osmose-Class3) / nb(connection nodes) | Connection nodes computed by grid analysis"
+
+    if nb_conn_line_sub > 0:
+        key = "health_consistent_linesub_voltage_connection"
+        names[key] = "Line-Substation voltage consistency (Osmose&nbsp;Class&nbsp;7)"
+        indicators[key] = 1 - data["class"]["7"] / nb_conn_line_sub
+        explanations[key] = "1 - nb(Osmose-Class7) / nb(line-sub connection) | Line-Sub connection computed by grid analysis"
+
+    indicators_p = {key: round(100*val,1) for key, val in indicators.items()}
+
+    output_data = []
+    for key in indicators.keys():
+        output_data.append({"key":key, "name":names.get(key, ""), "value":indicators_p.get(key, ""), "explain":explanations.get(key, "")})
+
+    ## Computing Stats indicator
+    names = {}
+    indicators = {}
+    explanations = {}
+
+    key = "stats_nb_international_connections"
+    names[key] = "Number of international connection"
+    indicators[key] = len([n for n in G.nodes if G.nodes[n]["grid_role"] == "international"])
+    explanations[key] = "..."
+
+    key = "stats_nb_substations"
+    names[key] = "Number of substations"
+    indicators[key] = len([n for n in G.nodes if G.nodes[n]["grid_role"] == "substation"])
+    explanations[key] = "..."
+
+    key = "stats_line_voltages"
+    names[key] = "Lines voltages"
+    indicators[key] = df_power_line["voltage"].unique().tolist()
+    explanations[key] = "..."
+
+    for key in indicators.keys():
+        output_data.append({"key":key, "name":names.get(key, ""), "value":indicators.get(key, ""), "explain":explanations.get(key, "")})
+
+    #import pprint
+    #pprint.pp(output_data)
+
+    with open(f"build/quality_score_{country_code}.json", "w", encoding="utf-8") as f:
+        json.dump(output_data, f, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+    main("NZ")
+    """for ccode in WORLD_COUNTRY_DICT.keys():
+        try:
+            main(ccode)
+            print(ccode)
+        except Exception as e:
+            print("Error with", ccode)
+            pass"""
--- indicators_map/merging_data.py
+++ indicators_map/merging_data.py
@@ -1,24 +1,132 @@
 import geopandas as gpd
 import pandas as pd
+import numpy as np
+import ast
 from config import WORLD_COUNTRY_DICT
+import random
 
-filepath_world = "data/World Bank Official Boundaries - Admin 0 - Simplified.gpkg"
-filepath_voltage = "data/voltage_osm_line.xlsx"
-filepath_wikidata = "data/wikidata_countries_info_brut.csv"
+def gradient_color(t: float) -> str:
+    """
+    Retourne la couleur hexadécimale correspondant à la valeur t (0 ≤ t ≤ 1)
+    selon le gradient défini.
+    """
+    # Stops définis comme (offset, (R,G,B))
+    if t<=0:
+        return "#AAAAAA"
+
+    stops = [
+        (0.0,   (0, 0, 0) ),
+        (0.1111111111111111, (23, 12, 1)),
+        (0.2222222222222222, (47, 24, 2)),
+        (0.3333333333333333, (56, 29, 2)),
+        (0.4444444444444444, (80, 40, 14)),
+        (0.5555555555555556, (124, 54, 28)),
+        (0.6666666666666666, (177, 104, 49)),
+        (0.7777777777777778, (210, 166, 62)),
+        (0.8888888888888888, (242, 229, 76)),
+        (1.0,  (42, 163, 100)),
+    ]
+
+    stops = [
+        (0.0,   (255, 0, 0) ),
+        (0.75, (255, 255, 0)),
+        (1.0,  (0, 255, 0)),
+    ]
+
+    # Clamp t dans [0,1]
+    t = max(0.0, min(1.0, t))
+
+    # Trouver les deux stops encadrant t
+    for i in range(len(stops) - 1):
+        t0, c0 = stops[i]
+        t1, c1 = stops[i + 1]
+        if t0 <= t <= t1:
+            # interpolation linéaire
+            ratio = (t - t0) / (t1 - t0) if t1 != t0 else 0
+            r = round(c0[0] + (c1[0] - c0[0]) * ratio)
+            g = round(c0[1] + (c1[1] - c0[1]) * ratio)
+            b = round(c0[2] + (c1[2] - c0[2]) * ratio)
+            return f"#{r:02X}{g:02X}{b:02X}"
+
+    # Cas extrême : si t == 1
+    return f"#{stops[-1][1][0]:02X}{stops[-1][1][1]:02X}{stops[-1][1][2]:02X}"
+
+filepath_world = "data/country_worldmap/custom.geojson"
+filepath_voltage = "data/osm_country_data_power_line.xlsx"
+filepath_wikidata = "data/wikidata_countries_info_formatted.csv"
 filepath_openinframap = "data/openinframap_countries_info_brut.csv"
+filepath_score = "data/0_health_score.xlsx"
 
-gdf_world = gpd.read_file(filepath_world)
-df_voltage = pd.read_excel(filepath_voltage)
-df_wikidata = pd.read_csv(filepath_wikidata)
-df_openinframap = pd.read_csv(filepath_openinframap)
+gdf_world = gpd.read_file(filepath_world, na_filter=False)
+df_voltage = pd.read_excel(filepath_voltage, na_filter=False)
+df_wikidata = pd.read_csv(filepath_wikidata, na_filter=False)
+df_openinframap = pd.read_csv(filepath_openinframap, na_filter=False)
+df_score = pd.read_excel(filepath_score, na_filter=False)
 
 print(gdf_world.columns)
 print(df_voltage.columns)
 print(df_wikidata.columns)
 print(df_openinframap.columns)
+print(df_score.columns)
 
-gdf_world = gdf_world.merge(df_voltage, left_on='ISO_A2', right_on='Country Code', suffixes=(None, "_voltage"), how='left')
-gdf_world = gdf_world.merge(df_wikidata, left_on='ISO_A2', right_on='codeiso2', suffixes=(None, "_wikidata"), how='left')
-gdf_world = gdf_world.merge(df_openinframap, left_on='ISO_A2', right_on='codeiso2', suffixes=(None, "_openinframap"), how='left')
+gdf_world = gdf_world.merge(df_voltage, left_on='iso_a2_eh', right_on='Country Code', suffixes=(None, "_voltage"), how='left')
+gdf_world = gdf_world.merge(df_wikidata, left_on='iso_a2_eh', right_on='codeiso2', suffixes=(None, "_wikidata"), how='left')
+gdf_world = gdf_world.merge(df_openinframap, left_on='iso_a2_eh', right_on='codeiso2', suffixes=(None, "_openinframap"), how='left')
+gdf_world = gdf_world.merge(df_score, left_on='iso_a2_eh', right_on='codeiso2', suffixes=(None, "_score"), how='left')
+
+
+print("-------------------")
+import pprint
+pprint.pp(list(gdf_world.columns))
+
+print(gdf_world.iloc[0])
+
+gdf_world["code_isoa2"] = gdf_world["iso_a2_eh"]
+gdf_world["name"] = gdf_world["Country Name"]
+
+gdf_world["quality_score"] = gdf_world['Line voltage'].apply(lambda x: random.random())
+gdf_world["quality_color"] = gdf_world['quality_score'].apply(lambda x: gradient_color(x))
+gdf_world["quality_score"] = np.where(gdf_world["name"].isna(), -1, gdf_world["quality_score"] )
+gdf_world["quality_color"] = np.where(gdf_world["name"].isna(), "#AAAAAA", gdf_world["quality_color"] )
+
+gdf_world["temp_line_voltage"] = gdf_world['Line voltage']
+gdf_world["temp_line_voltage"] = np.where(gdf_world["temp_line_voltage"].isna(), "[]", gdf_world["temp_line_voltage"])
+gdf_world["temp_line_voltage"] = gdf_world["temp_line_voltage"].map(ast.literal_eval)
+gdf_world["line_voltage"] = gdf_world["temp_line_voltage"].apply(lambda x: ", ".join([str(int(int(j)/1000)) + " kV" for j in x if int(j) > 50000])).astype(str)
+
+health_score_cols = ['health_power_line_connectivity',
+ 'health_grid_connectivity_without_circuit',
+ 'health_grid_connectivity_with_circuit',
+ 'health_line_voltage_completeness',
+ 'health_line_cables_completness',
+ 'health_substation_voltage_completness',
+ 'health_connected_power_tower',
+ 'health_complete_power_line',
+ 'health_consistent_line_voltage_connection',
+ 'health_consistent_linesub_voltage_connection']
+
+other_cols = [
+ 'stats_nb_international_connections',
+ 'stats_nb_substations',
+ 'stats_line_voltages',]
+
+select_columns = ["code_isoa2", "name", "flag_image", "osm_rel_id", "population", "area_km2", "gdp_bd", "power_line_total_length",
+                  "power_plant_count", "power_plant_output_mw", "line_voltage",
+                  "quality_score", "quality_color",
+                  'geometry'] + other_cols + health_score_cols
+
+gdf_world = gdf_world[select_columns]
+
+for col in health_score_cols:
+    gdf_world[col] = gdf_world[col].apply(lambda x: float(x) if x else 0)
+    gdf_world[col] = np.where(gdf_world[col].isna(),
+                              0, gdf_world[col])
+
+gdf_world["health_score_overall"] = 0
+for col in health_score_cols:
+    gdf_world["health_score_overall"] += gdf_world[col]
+gdf_world["health_score_overall"] /= len(health_score_cols)
+gdf_world["quality_score"] = gdf_world["health_score_overall"] / 100
+gdf_world["quality_color"] = gdf_world["quality_score"].map(gradient_color)
 
 gdf_world.to_file("data/worldmap_indicators.geojson")
