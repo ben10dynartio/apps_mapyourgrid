d48e3f9704f76d41a50366adbf2436ca48e0e59c
B.Ribon <b.ribon@dynartio.com>
2025-12-31 23:44:14 +0100

    upgrade

--- merge_world/buildworldmap.py
+++ merge_world/buildworldmap.py
@@ -99,13 +99,13 @@ print(df_openinframap.columns)
 print(df_health_score.columns)
 print(df_coverage_score.columns)"""
 
-gdf_world = gdf_world.merge(df_voltage, left_on='ISO_A2_EH', right_on='codeiso2', suffixes=(None, "_voltage"), how='left')
-gdf_world = gdf_world.merge(df_wikidata, left_on='ISO_A2_EH', right_on='codeiso2', suffixes=(None, "_wikidata"), how='left')
-gdf_world = gdf_world.merge(df_openinframap, left_on='ISO_A2_EH', right_on='codeiso2', suffixes=(None, "_openinframap"), how='left')
-gdf_world = gdf_world.merge(df_health_score, left_on='ISO_A2_EH', right_on='codeiso2', suffixes=(None, "_health_score"), how='left')
-gdf_world = gdf_world.merge(df_coverage_score, left_on='ISO_A2_EH', right_on='codeiso2', suffixes=(None, "_coverage_score"), how='left')
-gdf_world = gdf_world.merge(df_circuit_length, left_on='ISO_A2_EH', right_on='codeiso2', suffixes=(None, "_circuit_length"), how='left')
-gdf_world = gdf_world.merge(df_circuit_length_comparison, left_on='ISO_A2_EH', right_on='codeiso2', suffixes=(None, "_circuit_length_comparison"), how='left')
+gdf_world = gdf_world.merge(df_voltage, left_on='ISO_A2', right_on='codeiso2', suffixes=(None, "_voltage"), how='left')
+gdf_world = gdf_world.merge(df_wikidata, left_on='ISO_A2', right_on='codeiso2', suffixes=(None, "_wikidata"), how='left')
+gdf_world = gdf_world.merge(df_openinframap, left_on='ISO_A2', right_on='codeiso2', suffixes=(None, "_openinframap"), how='left')
+gdf_world = gdf_world.merge(df_health_score, left_on='ISO_A2', right_on='codeiso2', suffixes=(None, "_health_score"), how='left')
+gdf_world = gdf_world.merge(df_coverage_score, left_on='ISO_A2', right_on='codeiso2', suffixes=(None, "_coverage_score"), how='left')
+gdf_world = gdf_world.merge(df_circuit_length, left_on='ISO_A2', right_on='codeiso2', suffixes=(None, "_circuit_length"), how='left')
+gdf_world = gdf_world.merge(df_circuit_length_comparison, left_on='ISO_A2', right_on='codeiso2', suffixes=(None, "_circuit_length_comparison"), how='left')
 
 """print("-------------------")
 import pprint
@@ -113,7 +113,7 @@ pprint.pp(list(gdf_world.columns))
 
 print(gdf_world.iloc[0])"""
 
-gdf_world["code_isoa2"] = gdf_world["ISO_A2_EH"]
+gdf_world["code_isoa2"] = gdf_world["ISO_A2"]
 gdf_world["name"] = gdf_world["countryLabel"]
 gdf_world["name"] = np.where(gdf_world["name"].isna(),
                              gdf_world["ADMIN"], gdf_world["name"])
